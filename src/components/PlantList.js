582d45da073f62239c7ec9226096bc596aec6303
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-12 03:19:09 -0500

    first commit

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+  state = [];
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
