ab39608ddf73ba0b603fdde4a868c54fafab2856
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-04-17 11:46:18 -0400

    plants are displaying on PlantList component page

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,7 +2,23 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => {
+        this.setState({
+          plants: res.data,
+        });
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
